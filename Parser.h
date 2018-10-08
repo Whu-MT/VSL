8f4f1c20b5ad084a194181fc0ee1e4e185461c59
七六 <641733686@qq.com>
2018-10-08 16:38:43 +0800

    数字，变量，二元表达式的codegen

--- Parser.h
+++ Parser.h
@@ -3,6 +3,8 @@
 #include "Lexer.h"
 #include "AST.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Verifier.h"
+using namespace llvm;
 
 static int CurTok;
 static std::map<char, int> BinopPrecedence;
@@ -289,4 +291,39 @@ Value *LogErrorV(const char *Str) {
 	LogError(Str);
 	return nullptr;
 }
+
+Value *NumberExprAST::codegen() {
+	return ConstantFP::get(TheContext, APFloat((double)Val));
+}
+
+Value *VariableExprAST::codegen() {
+	// Look this variable up in the function.
+	Value *V = NamedValues[Name];
+	if (!V)
+		return LogErrorV("Unknown variable name");
+	return V;
+}
+
+Value *BinaryExprAST::codegen() {
+	Value *L = LHS->codegen();
+	Value *R = RHS->codegen();
+	if (!L || !R)
+		return nullptr;
+
+	switch (Op) {
+	case '+':
+		return Builder.CreateFAdd(L, R, "addtmp");
+	case '-':
+		return Builder.CreateFSub(L, R, "subtmp");
+	case '*':
+		return Builder.CreateFMul(L, R, "multmp");
+	case '/':
+		L = Builder.CreateFDiv(L, R, "divtmp");
+		// Convert bool 0/1 to double 0.0 or 1.0
+		return Builder.CreateUIToFP(L, Type::getDoubleTy(TheContext), "booltmp");
+	default:
+		return LogErrorV("invalid binary operator");
+	}
+}
+
 #endif
